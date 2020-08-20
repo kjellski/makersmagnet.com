481828c53a63783959c7775319ea568c3fc484e8
Kjell Otto EXT <info@kjell-otto.de>
2020-08-20 12:35:31 +0200

    feat: add service worker

---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{html,css,ico,eot,svg,ttf,woff,otf,less,scss,png,jpg,js,xml}"
+  ],
+  "swDest": "static/service-worker.js"
+};
